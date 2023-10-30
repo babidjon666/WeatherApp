f667ac45e7a3ed59faf075157e1b3e55673cb89a
MarkPolyakov <markpolya@icloud.com>
2023-10-30 17:01:23 +0500

    wether card (no model)

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -13,6 +13,55 @@ struct ContentView: View {
             Image("background")
                 .resizable()
                 .ignoresSafeArea()
+            
+            VStack {
+                
+                header
+                
+                ZStack{
+                    VStack{
+                        VStack {
+                            Text("Moscow") // name of City
+                                .font(.title2)
+                            .bold()
+                            
+                            Text("Tuesday, July 05, 2016") // date
+                                .font(.subheadline)
+                        }
+                        .padding()
+                        
+                        Text("26° C") // gradus
+                            .padding()
+                            .bold()
+                            .font(.largeTitle)
+                        
+                        Spacer()
+                    }
+                    .frame(width: 300,height: 400)
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    
+                    Button(action: {
+                        // add to list
+                    }, label: {
+                        Circle()
+                            .padding(.top, 400)
+                            .frame(width: 60)
+                            .foregroundColor(.white)
+                            .shadow(radius: 10)
+                            .overlay{
+                                Image(systemName: "plus")
+                                    .padding(.top, 400)
+                                    .font(.title3)
+                                    .bold()
+                                    .foregroundColor(.gray)
+                            }
+                    })
+                }
+                .padding(20)
+                
+                Spacer()
+            }
         }
         
     }
@@ -20,4 +69,35 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+extension ContentView{
+    
+    var header: some View{
+        HStack(spacing:100){
+            Button(action: {
+                //
+            }, label: {
+                Image(systemName: "list.bullet")
+                    .bold()
+                    .foregroundColor(.white)
+            })
+            
+            
+            Text("Weather")
+                .bold()
+                .foregroundColor(.white)
+                .font(.title2)
+            
+            Button(action: {
+                //
+            }, label: {
+                Text("•••")
+                    .bold()
+                    .foregroundColor(.white)
+                    .font(.title2)
+            })
+        }
+        .padding()
+    }
 }
